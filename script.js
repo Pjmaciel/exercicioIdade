1877264281b0da98944ed34e8a916c881f8e48fc
Pjmaciel <pjmaciel_4@hotmail.com>
2023-08-19 17:16:58 -0300

    Create Project

---
+++ script.js
@@ -0,0 +1,53 @@
+// Declare a variável img fora das funções para que ela seja acessível em ambos os lugares
+const img = document.createElement('img');
+img.setAttribute('id', 'foto');
+
+function verificar() {
+  const data = new Date();
+  const ano = data.getFullYear();
+  const formularioAno = document.getElementById("txtano");
+  const resultado = document.querySelector("div#resultado");
+  const fsex = document.getElementsByName("radsex");
+
+  if (formularioAno.value.length == 0 || Number(formularioAno.value) > ano) {
+    window.alert("Verifique os dados e tente novamente");
+  } else {
+    const idade = ano - Number(formularioAno.value);
+    let genero = '';
+
+    if (fsex[0].checked) {
+        genero = 'Homem';
+    } else if (fsex[1].checked) {
+        genero = 'Mulher';        
+    }
+    
+    verificarIdade(idade, genero); // Chame a função verificarIdade() passando a idade e o gênero
+    resultado.style.textAlign = 'center';
+    resultado.innerHTML = `Detectamos ${genero} com ${idade} anos.`;
+    resultado.appendChild(img);
+  }
+}
+
+function verificarIdade(idade, genero) {
+    if (genero === 'Homem') {
+        if (idade >= 0 && idade < 10) {
+            img.setAttribute('src', 'bebem.png');
+        } else if (idade < 21) {
+            img.setAttribute('src', 'jovemm.png');
+        } else if (idade < 60) {
+            img.setAttribute('src', 'adulto.png');
+        } else {
+            img.setAttribute('src', 'velho.png');
+        }
+    } else if (genero === 'Mulher') {
+        if (idade >= 0 && idade < 10) {
+            img.setAttribute('src', 'bebef.png');
+        } else if (idade < 21) {
+            img.setAttribute('src', 'jovemf.png');
+        } else if (idade < 60) {
+            img.setAttribute('src', 'adulta.png');
+        } else {
+            img.setAttribute('src', 'velha.png');
+        }
+    }
+}
